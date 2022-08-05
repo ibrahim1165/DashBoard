93a90c98350fa3a3e319685d8087af06439dc4b9
Md Ibrahim <mdrafi15429@gmail.com>
2022-08-05 19:16:58 +0600

    config icone added

--- src/pages/Config/Config.js
+++ src/pages/Config/Config.js
@@ -1,9 +1,46 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiTwotoneHome} from "react-icons/ai";
+import { FiLock} from "react-icons/fi";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { BsBellFill } from "react-icons/bs";
+import { BsLink45Deg} from "react-icons/bs";
 const Config = () => {
     return (
         <div>
-            <h2>He4llo world</h2>
+          <div className="flex items-center">
+          <h1 className="text-3xl px-6">Configuration</h1>
+          <Link className="text-xl text-primary flex items-center" to="/">
+            <AiTwotoneHome />
+            DashBoards</Link>
+          </div>
+           <div class="navbar bg-base-100">
+        <div class="navbar-center mt-4 text-black text-xl lg:flex">
+          <ul class="menu menu-horizontal p-0">
+            <li><a>
+          <AiOutlineUser />
+          Categories
+                </a></li>
+            <li><a>
+               <FiLock />
+               Quicklist
+                </a></li>
+            <li><a>
+             <BsFillBookmarkFill />
+              Department
+                </a></li>
+            <li><a>
+             <BsBellFill />
+             Country
+                </a></li>
+            <li><a>
+               <BsLink45Deg />
+               Urgency
+                </a></li>
+          </ul>
+        </div>
+      </div>
         </div>
     );
 };
